836ac93fb5f0c93234e2ed43b930b88e9fa11143
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-18 22:38:01 +0545

    Implemented listerner for buttons

--- Todo_DetailApp/app/src/main/java/com/example/todo_detail_app/TodoActivity.java
+++ Todo_DetailApp/app/src/main/java/com/example/todo_detail_app/TodoActivity.java
@@ -16,21 +16,14 @@ public class TodoActivity extends AppCompatActivity {
 
     public static final String TAG = "TodoActivity";
     private static final int IS_SUCCESS = 0;
-    /* map or name, value pair to be returned in an intent */
+
     private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
-    /**
-     * In case of state change, due to rotating the phone
-     * store the mTodoIndex to display the same mTodos element post state change
-     * N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
-     * the alternative is to abstract view data to a ViewModel which can be in scope in all
-     * Activity states and more suitable for larger amounts of data
-     */
 
     private static final String TODO_INDEX = "com.example.todoIndex";
     private String[] mTodos;
     private int mTodoIndex = 0;
 
-    /* override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key */
+
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -39,19 +32,12 @@ public class TodoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        /* call the super class onCreate to complete the creation of Activity
-           like the view hierarchy */
         super.onCreate(savedInstanceState);
 
-        Log.d(TAG, " **** Just to say the PC is in onCreate!");
 
-        /* set the user interface layout for this Activity
-         the layout file is defined in the project res/layout/activity_todo.xml file */
+
         setContentView(R.layout.activity_todo);
 
-        /* check for saved state due to changes such as rotation or back button
-           and restore any saved state such as the todo_index */
         if (savedInstanceState != null) {
             mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
         }
